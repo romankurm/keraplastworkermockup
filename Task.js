ea7320f39423f6b27c51119894600dd5fa8bb216
romankurm <romankurm9@gmail.com>
2026-06-19 15:51:34 +0300

    Delete Task.js

--- Task.js
+++
@@ -1,8 +0,0 @@
-export class Task {
-    constructor(guid, operation, status, startDate) {
-        this.guid = guid;
-        this.operation = operation;
-        this.status = status;
-        this.startDate = startDate;
-    }
-}
